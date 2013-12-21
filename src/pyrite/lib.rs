4dd466d91c0b825d4e33fb3b2f3744fb1ed4736c
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-12-21 18:39:29 -0500

    Add basic channel example.

--- src/pyrite/lib.rs
+++ src/pyrite/lib.rs
@@ -1,6 +1,25 @@
 #[desc = "The Pyrite Rust package."];
 #[license = "MIT"];
 
-pub fn world() {
-  println("Hello, world.");
+extern mod extra;
+
+use extra::comm::DuplexStream;
+
+fn main() {
+  let (server, client) = DuplexStream();
+
+  do spawn {
+    let mut buffer: ~str;
+
+    loop {
+      buffer = server.recv();
+      println(format!("Closure received: {:s}", buffer));
+      server.send(buffer);
+    }
+  }
+
+  do 5.times {
+    client.send(~"Hello");
+    println(format!("Server received back: {:s}", client.recv()));
+  }
 }
