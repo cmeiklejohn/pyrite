bc40bf31276a6298d5486e5d6942b2135c9d893f
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-12-21 22:49:03 -0500

    Commit WIP.

--- src/pyrite/backend.rs
+++ src/pyrite/backend.rs
@@ -1,56 +1,60 @@
 #[desc = "Backend implementation for Pyrite"];
 #[license = "MIT"];
 
-use std::hashmap::HashMap;
+mod backend {
 
-trait Backend {
-  fn new() -> Self;
-  fn get(&self, key: ~str) -> Option<~str>;
-  fn put(&mut self, key: ~str, value: ~str) -> bool;
-}
+  use std::hashmap::HashMap;
 
-struct MemoryBackend {
-  reference: HashMap<~str, ~str>
-}
-
-impl Backend for MemoryBackend {
-  fn new() -> MemoryBackend {
-    MemoryBackend { reference: HashMap::new() }
+  trait Backend {
+    fn new() -> Self;
+    fn get(&self, key: ~str) -> Option<~str>;
+    fn put(&mut self, key: ~str, value: ~str) -> bool;
   }
 
-  fn get(&self, key: ~str) -> Option<~str> {
-    self.reference.find_copy(&key)
+  pub struct MemoryBackend {
+    reference: HashMap<~str, ~str>
   }
 
-  fn put(&mut self, key: ~str, value: ~str) -> bool {
-    self.reference.insert(key.clone(), value.clone())
-  }
-}
+  impl Backend for MemoryBackend {
+    fn new() -> MemoryBackend {
+      MemoryBackend { reference: HashMap::new() }
+    }
 
-#[test]
-fn basic_test() {
-  let mut backend: MemoryBackend = Backend::new();
+    fn get(&self, key: ~str) -> Option<~str> {
+      self.reference.find_copy(&key)
+    }
 
-  // Verify we can put an object.
-  assert!(backend.put(~"key", ~"value"));
-
-  // Verify we can retrieve the objecta after putting.
-  match backend.get(~"key") {
-    Some(value) => {
-      assert!(~"value" == value)
-    },
-    None => {
-      fail!("Key could not be found after writing!")
+    fn put(&mut self, key: ~str, value: ~str) -> bool {
+      self.reference.insert(key.clone(), value.clone())
     }
   }
 
-  // Verify we get an error for an object that isn't there.
-  match backend.get(~"missing") {
-    None => {
-      assert!(true)
-    },
-    _ => {
-      fail!("Found an object we didn't write!")
+  #[test]
+  fn basic_test() {
+    let mut backend: MemoryBackend = Backend::new();
+
+    // Verify we can put an object.
+    assert!(backend.put(~"key", ~"value"));
+
+    // Verify we can retrieve the objecta after putting.
+    match backend.get(~"key") {
+      Some(value) => {
+        assert!(~"value" == value)
+      },
+      None => {
+        fail!("Key could not be found after writing!")
+      }
+    }
+
+    // Verify we get an error for an object that isn't there.
+    match backend.get(~"missing") {
+      None => {
+        assert!(true)
+      },
+      _ => {
+        fail!("Found an object we didn't write!")
+      }
     }
   }
+
 }
