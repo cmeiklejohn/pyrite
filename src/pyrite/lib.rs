6e96e91261a0b4ba7df9e5fe0048d7c7a7153e92
Christopher Meiklejohn <christopher.meiklejohn@gmail.com>
2013-12-21 19:34:49 -0500

    Add simple k/v backend storage system.

--- src/pyrite/lib.rs
+++ src/pyrite/lib.rs
@@ -3,23 +3,94 @@
 
 extern mod extra;
 
+use std::hashmap::HashMap;
 use extra::comm::DuplexStream;
 
+enum BackendRequestMessage {
+  BackendTerminate(),
+  BackendGetRequest(~str),
+  BackendStoreRequest(~str, ~str)
+}
+
+enum BackendResponseMessage {
+  BackendGetFailedResponse(~str),
+  BackendGetSuccessResponse(~str, ~str),
+  BackendStoreFailedResponse(~str, ~str),
+  BackendStoreSuccessResponse(~str, ~str)
+}
+
+/// Memory based backend for a simplistic key/value store.
+fn backend(channel: &DuplexStream<BackendResponseMessage, BackendRequestMessage>) {
+  let mut backend: HashMap<~str, ~str>;
+  let mut message: BackendRequestMessage;
+
+  backend = HashMap::new();
+
+  loop {
+    message = channel.recv();
+
+    match message {
+      BackendGetRequest(key) => {
+        println(format!("Request to retrieve key {:s}", key));
+        match backend.find_copy(&key) {
+          Some(value) => {
+            channel.send(BackendGetSuccessResponse(key, value));
+          },
+          _ => {
+            channel.send(BackendGetFailedResponse(key));
+          }
+        }
+      }
+      BackendStoreRequest(key, value) => {
+        println(format!("Request to store key/value pair {:s} => {:s}",
+                        key, value));
+        match backend.insert(key.clone(), value.clone()) {
+          true => {
+            channel.send(BackendStoreSuccessResponse(key, value))
+          },
+          false => {
+            channel.send(BackendStoreFailedResponse(key, value))
+          }
+        };
+      },
+      BackendTerminate() => {
+        println(format!("Terminating backend."));
+        break
+      }
+    }
+  }
+}
+
+// Perform a basic test of the key/value store.
 fn main() {
   let (server, client) = DuplexStream();
 
   do spawn {
-    let mut buffer: ~str;
+    backend(&server);
+  }
 
-    loop {
-      buffer = server.recv();
-      println(format!("Closure received: {:s}", buffer));
-      server.send(buffer);
+  client.send(BackendStoreRequest(~"Ingmar Bergman", ~"The Silence"));
+  client.send(BackendStoreRequest(~"Jean-Luc Godard", ~"Breathless"));
+
+  client.send(BackendGetRequest(~"Jean-Luc Godard"));
+  client.send(BackendGetRequest(~"Francois Truffaut"));
+
+  do 3.times {
+    match client.recv() {
+      BackendGetFailedResponse(key) => {
+        println(format!("Key {:s} retrieval failed!", key));
+      },
+      BackendGetSuccessResponse(key, value) => {
+        println(format!("Key {:s} retrieval successful => {:s}!", key, value));
+      },
+      BackendStoreFailedResponse(key, _) => {
+        println(format!("Key {:s} store failed!", key));
+      },
+      BackendStoreSuccessResponse(key, _) => {
+        println(format!("Key {:s} stored successfully!", key));
+      }
     }
   }
 
-  do 5.times {
-    client.send(~"Hello");
-    println(format!("Server received back: {:s}", client.recv()));
-  }
+  client.send(BackendTerminate);
 }
